c0fac8348d6be05fe487a2ab9c931c7968a94380
Qwizer <romario.sr@live.com>
2018-11-20 11:28:30 -0200

    Update definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -20,15 +20,15 @@
 #ifndef FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 #define FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 
-static constexpr auto STATUS_SERVER_NAME = "OTX Server";
+static constexpr auto STATUS_SERVER_NAME = "OTX Server and Forgotten Server";
 static constexpr auto STATUS_SERVER_VERSION = "3";
 static constexpr auto MINOR_VERSION = "10";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "Matt Gomez, Leandro Brewster, Erick Nunes, Joao Leonardo Pereira, Bruno Carvalho, Charles Kassab, OTX Contributors and The Forgotten Server Developers";
-static constexpr auto GIT_REPO = "https://github.com/qwizer/otxserver/tree/otxserv3/CHANGELOG";
+static constexpr auto GIT_REPO = "https://github.com/Qwizer/realmap11";
 
 static constexpr auto CLIENT_VERSION_MIN = 1100;
-static constexpr auto CLIENT_VERSION_MAX = 1190;
-static constexpr auto CLIENT_VERSION_STR = "11.90";
+static constexpr auto CLIENT_VERSION_MAX = 1195;
+static constexpr auto CLIENT_VERSION_STR = "11.95";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
